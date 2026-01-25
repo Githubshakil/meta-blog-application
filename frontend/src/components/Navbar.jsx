8d7f5d7f296d778df6c46e2d2ada6a15ce5f02f5
Githubshakil <shakilmahmud007@gmail.com>
2026-01-26 01:59:51 +0600

    update navbar
    responsive navbar for mobile device

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import { NavLink } from "react-router";
 import Search from "./Search";
 import { FaMoon, FaSun } from "react-icons/fa";
-
+import { IoMdClose } from "react-icons/io";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
 
 const Navbar = () => {
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    // ToDo: use context here instead
-    const [isDarkMode, setIsDarkMode] = useState(false)
-    const toggleDarkMode = () => setIsDarkMode(!isDarkMode)
+  // ToDo: use context here instead
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
   return (
     <nav className="bg-white shadow-md">
       {/* desktop menu and mobile button */}
@@ -67,24 +71,88 @@ const Navbar = () => {
 
         {/*search and color toggle */}
         <div className="md:flex hidden items-center space-x-4">
-            {/* search box */}
-            <div>
-                <Search />
-            </div>
-                {/* color switcher */}
-            <div onClick={toggleDarkMode} className={`w-14 h-8 flex items-center bg-[#E8E8EA] rounded-full p-1 cursor-pointer transition-colors duration-300 ${isDarkMode ? "justify-end" : "justify-start"}`}>
-               <button onClick={toggleDarkMode} className="w-6 h-6 bg-white rounded-full shadow-md flex items-center justify-center transition-transform duration-300 cursor-pointer">
-                 {
-                    isDarkMode ? <FaMoon className="text-gray-500" /> : <FaSun className="text-yellow-500"  />
+          {/* search box */}
+          <div>
+            <Search />
+          </div>
+          {/* color switcher */}
+          <div
+            onClick={toggleDarkMode}
+            className={`w-14 h-8 flex items-center bg-[#E8E8EA] rounded-full p-1 cursor-pointer transition-colors duration-300 ${isDarkMode ? "justify-end" : "justify-start"}`}
+          >
+            <button
+              onClick={toggleDarkMode}
+              className="w-6 h-6 bg-white rounded-full shadow-md flex items-center justify-center transition-transform duration-300 cursor-pointer"
+            >
+              {isDarkMode ? (
+                <FaMoon className="text-gray-500" />
+              ) : (
+                <FaSun className="text-yellow-500" />
+              )}
+            </button>
+          </div>
+        </div>
 
-                }
-               </button>
-            </div>
+        {/* mobile menu bar */}
+        <div className="md:hidden block">
+          <button onClick={toggleMobileMenu}>
+            {isMenuOpen ? (
+              <IoMdClose className="size-6 text-gray-600" />
+            ) : (
+              <HiMiniBars3BottomRight className="size-6 text-gray-600" />
+            )}
+          </button>
         </div>
       </div>
 
       {/* Mobile menu items */}
-      <div className="hidden">Mobile menu items</div>
+      {isMenuOpen && (
+        <div className="md:hidden bg-white shadow-md">
+          <ul className="flex flex-col items-center space-y-4 text-gray-700 py-4">
+            <li onClick={toggleMobileMenu}>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) =>
+                  isActive ? "text-secondary" : "hover:text-secondary"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li onClick={toggleMobileMenu}>
+              <NavLink
+                to={"/blogs"}
+                className={({ isActive }) =>
+                  isActive ? "text-secondary" : "hover:text-secondary"
+                }
+              >
+                Blogs
+              </NavLink>
+            </li>
+            <li onClick={toggleMobileMenu}>
+              <NavLink
+                to={"/about"}
+                className={({ isActive }) =>
+                  isActive ? "text-secondary" : "hover:text-secondary"
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li onClick={toggleMobileMenu}>
+              <NavLink
+                to={"/contact"}
+                className={({ isActive }) =>
+                  isActive ? "text-secondary" : "hover:text-secondary"
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+          <Search/>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
