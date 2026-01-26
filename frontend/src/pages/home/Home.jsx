619080640cc00bb713a29f5a1b0b28185f47c9e4
Githubshakil <shakilmahmud007@gmail.com>
2026-01-27 01:07:14 +0600

    update newsletter section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import BlogList from '../blogs/BlogList'
+import NewsLetter from './NewsLetter'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
     <section className='container max-w-7xl mx-auto px-4 py-3'>
         <Banner />
         <BlogList/>
+        <NewsLetter />
     </section>
     </>
   )
