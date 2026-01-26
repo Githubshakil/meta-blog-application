f24d456152c707c482561b78576da71bd0485b08
Githubshakil <shakilmahmud007@gmail.com>
2026-01-26 18:58:20 +0600

    update blogs
    Show More Blog and filtering blog based on search

--- frontend/src/pages/blogs/BlogList.jsx
+++ frontend/src/pages/blogs/BlogList.jsx
@@ -2,25 +2,45 @@ import React, { useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
 
 const BlogList = () => {
+  const [searchTerm] = useState(""); //todo:use blog context
   const [blogs, setBlogs] = useState([]);
+  const [showBlogs, setShowBlogs] = useState(6);
   useEffect(() => {
     fetch("blogs.json")
-    .then((response) => response.json())
-    .then(data => setBlogs(data))
-    .catch(error => console.error("Error fetching blog data: " + error))
+      .then((response) => response.json())
+      .then((data) => setBlogs(data))
+      .catch((error) => console.error("Error fetching blog data: " + error));
   }, []);
 
-  console.log("Blogs:", blogs)
+  //filter blogs based on our title description author name
+
+  const filteredBlogs = blogs.filter(
+    (blog) =>
+      blog.title.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+      blog.description.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+      blog.author.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()),
+  );
+  // handle view more blog
+
+  const handleMoreBlogs = () => {
+    setShowBlogs((prev) => prev + 3);
+  };
   return (
     <>
       <div className=" container mx-auto">
         <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            {
-                blogs.map((blog, idx) => (
-                    <BlogCard key={idx} blog={blog}/>
-                ))
-            }
+          {filteredBlogs.slice(0,showBlogs).map((blog, idx) => (
+            <BlogCard key={idx} blog={blog} />
+          ))}
         </div>
+
+        {
+            showBlogs < filteredBlogs.length && (
+                <div onClick={handleMoreBlogs} className="flex justify-center items-center mt-8 mb-5">
+                    <button className="bg-secondary text-white hover:bg-secondary/80 font-extrabold flex items-center justify-center px-6 py-2 gap-1 transition-colors duration-200 rounded-md">View More</button>
+                </div>
+            )
+        }
       </div>
     </>
   );
