a3c92c5a9e7c09edd55b78602b94f3ff60899f0e
Githubshakil <shakilmahmud007@gmail.com>
2026-02-06 11:44:54 +0600

    install mongodb and mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000
+
+
+
+
+//mongoose configuration
+async function main() {
+  await mongoose.connect('mongodb+srv://shakilmahmud007_db_user:hPsScwR7zfyK0XP7@meta-blog-app.yzdgejo.mongodb.net/?appName=meta-blog-app');
+}
+
+main().then(() => console.log("Mongodb connected successfully!")).catch(err => console.log(err));
+
 
 //routs
 app.get('/', (req, res) => {
