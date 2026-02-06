749820965ba8ff0c5ce3cf2ed54586dc68b8a660
Githubshakil <shakilmahmud007@gmail.com>
2026-02-06 12:16:07 +0600

    install dotenv and useit

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,24 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000
+require('dotenv').config()
 
 
 
 
 //mongoose configuration
 async function main() {
-  await mongoose.connect('mongodb+srv://shakilmahmud007_db_user:hPsScwR7zfyK0XP7@meta-blog-app.yzdgejo.mongodb.net/?appName=meta-blog-app');
-}
-
-main().then(() => console.log("Mongodb connected successfully!")).catch(err => console.log(err));
-
-
-//routs
+  await mongoose.connect(process.env.MONGODB_URI);
+  //routs
 app.get('/', (req, res) => {
   res.send('Meta Blog Server is running....!')
 })
+
+
+}
+main().then(() => console.log("MongoDB connected successfully!")).catch(err => console.log(err));
+
+
 
 
 app.listen(port, () =>{
