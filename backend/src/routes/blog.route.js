795d3f45dc6c16868dfc58f21b4e29373088c8bb
Githubshakil <shakilmahmud007@gmail.com>
2026-02-07 00:54:49 +0600

    Get all blogs routes

--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -1,21 +1,32 @@
-const express = require('express')
-const Blog = require('../models/blog.model')
-const router = express.Router()
+const express = require("express");
+const Blog = require("../models/blog.model");
+const router = express.Router();
 
 
 
-router.get('/', (req, res) => {
-  res.send('From Blog Routes!')
-})
+//get all blogs
+router.get("/", async(req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.status(200).send({ message: "Blogs fetched successfully!", blogs });
+  } catch (error) {
+     console.log("Error fatching all blog", error)
+    res.status(500).send({ message: "Error fetching blogs", error });
+  }
+});
 
 //post a new blog
-router.post('/add-post', async(req, res) => {
+router.post("/add-post", async (req, res) => {
+  try {
     const newBlog = new Blog({
-        ...req.body
-    })
-    const blog = await newBlog.save()
-    res.status(200).send({message: "Blog added successfully!", blog})
-})
+      ...req.body,
+    });
+    const blog = await newBlog.save();
+    res.status(200).send({ message: "Blog added successfully!", blog });
+  } catch (error) {
+    console.log("Error Creating a new blog", error)
+    res.status(500).send({ message: "Error adding blog", error });
+  }
+});
 
-
-module.exports = router
+module.exports = router;
