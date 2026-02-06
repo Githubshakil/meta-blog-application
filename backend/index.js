7aaa4ba7d165f46700ca6a123ce9f62da3b71773
Githubshakil <shakilmahmud007@gmail.com>
2026-02-06 10:55:01 +0600

    Explained of the basic Server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
+//routs
 app.get('/', (req, res) => {
-  res.send('Hello Developers!')
+  res.send('Meta Blog Server is running....!')
 })
 
-app.listen(port, () => {
+
+app.listen(port, () =>{
   console.log(`Example app listening on port ${port}`)
 })
